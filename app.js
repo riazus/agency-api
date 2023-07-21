4b527ee3090d690c32f001d598cbc5f5b03d4c88
rusmanov <usmanov.riyaz22@gmail.com>
2023-07-21 13:02:01 +0200

    feat: created JWT module
    Added files for evaluate requests

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-const express = require("express");
-const connectToDb = require("./utils/connectToDb");
-const mongoose = require("mongoose");
 require("dotenv").config();
+const express = require("express");
+const handleJWT = require("./middleware/JWT");
+const connectToDb = require("./utils/connectToDb");
+const cors = require("cors");
+const { corsConfig } = require("./utils/corsConfig");
+const mongoose = require("mongoose");
 const port = process.env.PORT || 3200;
 
 connectToDb();
 
 // create the express server, 
 const app = express();
+
+app.use(handleJWT);
+
+app.use(cors(corsConfig));
 
 mongoose.connection.once("open", () => {
   app.listen(port, () => console.log(`Server running on port ${port}`));
