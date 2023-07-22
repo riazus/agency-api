10999ef45690ef6c14ca57b9145cce7670beb644
rusmanov <usmanov.riyaz22@gmail.com>
2023-07-22 10:38:04 +0200

    dev: replace user controller to protected folder

--- controllers/protected/user.controller.js
+++ controllers/protected/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../../models/User");
 
 const getAllUsers = async (req, res) => {
 	const users = await User.find({}, 'email _id user_type');
